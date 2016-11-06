25b1bc194f5de32168d3dd564a5cc57d6be4b9ac
Lauri <l.oksanen@ucl.ac.uk>
2016-11-06 15:34:42 +0000

    Added diff command and tweaked the descriptors

--- mcturtle-scratch.js
+++ mcturtle-scratch.js
@@ -28,16 +28,18 @@
     ext.tp = function() { getreq("tp"); };
     ext.pen = function(bid, bsub) { getreq("pen/" + bid +"/"+ bsub); };
     ext.say = function(str) { getreqenc("say", str); };
+    ext.diff = function() { getreq("diff"); };
     ext.superpower = function(str) { getreqenc("superpower", str); };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            [" ", "move %m.move_dir", "move"],
-            [" ", "turn %m.turn_dir", "move"],
+            [" ", "move %m.move_dir", "move", "forward"],
+            [" ", "turn %m.turn_dir", "move", "left"],
             [" ", "teleport to player", "tp"],
-            [" ", "set pen bid:%n bsub:%n", "pen", 0, 0],
+            [" ", "set pen block id:%n block data:%n", "pen", 0, 0],
             [" ", "say %s", "say", "Hello"],
+            [" ", "tell difference from player", "diff"],
             [" ", "superpower %s", "superpower", ""],
         ],
         menus: {
